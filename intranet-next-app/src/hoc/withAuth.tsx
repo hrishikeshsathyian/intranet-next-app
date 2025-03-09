8f79f9640d711b3b1020654ad22590b8a9fb61dc
Hrishikesh Sathyian <e1121418@u.nus.edu>
2025-03-10 00:20:18 +0800

    Fix any bug

--- intranet-next-app/src/hoc/withAuth.tsx
+++ intranet-next-app/src/hoc/withAuth.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client"
 
 import React, { ComponentType, useEffect } from 'react';
@@ -19,7 +20,7 @@ const withAuth = (WrappedComponent: ComponentType) => {
     }, [user, loading, router]);
 
     if (loading || !user) {
-      return <div>Loading...</div>;
+      return ;
     }
 
     return <WrappedComponent {...props} />;
