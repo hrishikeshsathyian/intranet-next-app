11d1ebc8d7083cfdbd3738a465a9baf907a4c1ae
Hrishikesh Sathyian <e1121418@u.nus.edu>
2025-03-25 13:35:33 +0800

    Fix landing page redirection

--- intranet-next-app/src/app/page.tsx
+++ intranet-next-app/src/app/page.tsx
@@ -1,17 +1,14 @@
 'use client';
 
 import { useRouter } from "next/navigation";
-
+import { useEffect } from "react";
 export default function Home() {
   const router = useRouter();
+  useEffect(() => {
+    router.push("/auth/login");
+  });
   return (
     <div className="flex items-center justify-center min-h-screen bg-orange-500">
-      <button
-        className="bg-white text-orange-500 font-semibold text-lg py-3 px-6 rounded transition-colors hover:bg-gray-200"
-        onClick={() => router.push("/auth/login")}
-      >
-        Take me to auth
-      </button>
     </div>
   );
 }
