0c7974cc8707fa0e69d80d2bd0444365cefc40f7
Hrishikesh Sathyian <e1121418@u.nus.edu>
2025-03-28 05:03:02 +0800

    Enable intranet

--- intranet-next-app/src/app/page.tsx
+++ intranet-next-app/src/app/page.tsx
@@ -1,13 +1,13 @@
 'use client';
 
-// import { useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 export default function Home() {
-  // const router = useRouter();
+  const router = useRouter();
   useEffect(() => {
-    console.log("Uncomment to enable intranet");
-
-  });
+    router.push("/auth/login");
+  }, [router]);
+  
   return (
     <div className="flex items-center justify-center min-h-screen bg-orange-500">
     </div>
